d6274650c4cf4b69d3ac4fb95254148298a90784
Dane <info.danewanke@gmail.com>
2022-11-23 13:06:54 -0600

    added conditional rendering to typeSelector from bottles.json

--- src/components/bottle.js
+++ src/components/bottle.js
@@ -5,7 +5,7 @@ export default class Bottle extends React.Component {
     render() {
         return(
             <button className="BottleBtn">
-                <img src={this.props.bottle.photo} className='BottlePic' />
+                <img src={this.props.bottle.photo} className='BottlePic'/>
             </button>
         )
     }
